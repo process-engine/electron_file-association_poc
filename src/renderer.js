4270d0bdf1df6c19904b439e007be2f836fc6486
Miguel Carballo Garcia <miguel.garcia@5minds.de>
2018-03-21 16:43:06 +0100

    :lipstick: Fix Typo

--- src/renderer.js
+++ src/renderer.js
@@ -1,6 +1,6 @@
 if (window.nodeRequire) {
   const ipcRenderer = window.nodeRequire("electron").ipcRenderer;
-  const fileInfo= ipcRenderer.sendSync("get_opened_file");
+  const fileInfo = ipcRenderer.sendSync("get_opened_file");
 
   if (fileInfo !== undefined) {
     document.getElementById('filepath').innerText = fileInfo.path;
